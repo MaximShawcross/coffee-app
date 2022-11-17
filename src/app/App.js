3f2232c1b11fea451f40cd55ca9baabda90ffab6
Kushnir Maxim <maxbasta2006@gmail.com>
2022-11-17 23:54:20 +0100

    hashrout

--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import { fetchCoffeeList } from "../components/coffe-list/coffeSlice";
 
 import { MainPage, CoffeeListPage, SingleCoffeePage } from "../pages";
